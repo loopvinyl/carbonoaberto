83a14b77f0852228ab542990d3301913e85db739
loopvinyl <loopvinyl@gmail.com>
2025-06-12 19:47:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("""
 <style>
     .audio-accessibility {
         position: fixed;
-        top: 25px;
+        top: 55px;
         right: 15px;
         z-index: 1000;
         background: rgba(255, 255, 255, 0.95);
