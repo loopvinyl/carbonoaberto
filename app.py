b274a2281932a60f37b7340969f90e3b156ebcf9
loopvinyl <loopvinyl@gmail.com>
2025-06-12 11:56:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ df_anual = pd.read_excel(arquivo, sheet_name="Anual")
 
 # === Configuração da página ===
 st.set_page_config(page_title="Carbono Aberto", layout="wide")
-st.title("Carbono Aberto")
+st.title("CARBONO ABERTO")
 st.subheader("estima as Emissões Evitadas em tCO2e ao desviar resíduo com poda para compostagem, no lugar da destinação aterragem")
 
 # === KPIs ===
